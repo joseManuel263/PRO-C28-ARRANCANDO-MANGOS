6c3b0fe8fa7f41b905fd0f50959bb312a86f24f0
joseManuel263 <84207744+joseManuel263@users.noreply.github.com>
2021-07-14 19:21:58 -0500

    Add files via upload

---
+++ Launcher.js
@@ -0,0 +1,25 @@
+class Launcher {
+ constructor(body,anchor){
+    var options={
+        bodyA:body,
+        pointB:anchor,
+        stiffness:0.004,
+        length:1
+    }
+    this.bodyA=body;
+    this.pointB=anchor;
+    this.launcher=Constraint.create(options);
+    World.add(world,this.launcher);
+ }
+ attach(body){
+     this.launcher.bodyA=body;
+ }
+ fly(){
+     this.launcher.bodyA=null;
+ }
+ display(){
+     if(this.launcher.bodyA){
+         line(this.bodyA.position.x,this.bodyA.position.y,this.pointB.x,this.pointB.y);
+     }
+ }
+}
